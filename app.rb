4c1126172fc47c0ea62dc5c568c0790f1b42a2f7
karimabuseer <karimabuseer@gmail.com>
2021-07-14 15:12:13 +0100

    Added redirect to play page

--- app.rb
+++ app.rb
@@ -24,10 +24,12 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
-    erb(:play)
+    redirect to('/play')
   end
 
   get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
